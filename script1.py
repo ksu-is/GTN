5759265c1ec4876edc286c65157fabc3fcf775a8
cdionye93 <44480820+cdionye93@users.noreply.github.com>
2018-12-05 11:11:57 -0500

    Update script1.py

--- script1.py
+++ script1.py
@@ -6,14 +6,32 @@ def greet():
     ip = request.remote_addr
     inputName = inputName.upper()+" hi!  Visiting from" + str(ip)
     return render_template("home.html",myName=inputName)
+
+@app.route('/')
+def index():
+session['number'] = random.randrange(1,50)
+print session['number']
+return render_template("home.html")
+
+@app.route('/guess', methods=['POST'])
+def result():
+if request.form['guess'] == session['number']:
+    answer = "Good job! You guessed the number in " + numberofGuesses + " tries :)"
+    return render_template("home.html", answer=answer)
+elif int(request.form['guess']) < session['number']:
+    answer = "Your guess is too low! You have " + guessesLeft + " guesses left"
+    return render_template("home.html", answer=answer)
+else:
+    answer = "Your guess is too high! You have " + guessesLeft + " guesses left"
+      return render_template("index.html", answer=answer)
+
 @app.route('/')
 def home():
     
     return render_template("home.html",myName="")
 
-@app.route('/about/')
-def about():
-    return render_template("about.html")
+
 
 if __name__=="__main__":
     app.run(debug=True)
+
