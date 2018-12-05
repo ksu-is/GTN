71afddfa022da71122dd4138e7050cc68a3600ba
cdionye93 <44480820+cdionye93@users.noreply.github.com>
2018-12-05 12:17:38 -0500

    Update numbergame.py

--- numbergame.py
+++ numbergame.py
@@ -1,6 +1,7 @@
 #this is a number game where you guess the number. You get seven guesses to figure out a number between 1 and 50.
 import random
 
+#this is letting youu the numbers are between 1 and 50.
 numberofGuesses = 0
 number = random.randint(1,50)
 
@@ -15,10 +16,12 @@ while numberofGuesses < 8:
 	numberofGuesses = numberofGuesses + 1;
 	guessesLeft = 8 - numberofGuesses;
 
+#this lets you know how many guesses you have left and if you were too low.
 	if guess < number:
 		guessesLeft=str(guessesLeft)
 		print("Your guess is too low! You have " + guessesLeft + " guesses left")
 
+#this lets you know if you were too high and how many guesses you have left.
 	if guess > number:
 		guessesLeft=str(guessesLeft)
 		print("Your guess is too high! You have " + guessesLeft + " guesses left")
@@ -26,10 +29,12 @@ while numberofGuesses < 8:
 	if guess==number:
 		break
 
+#this lets you know that you guessed correctly and tell you how many guesses you used.
 if guess==number:
 	numberofGuesses=str(numberofGuesses)
 	print("Good job! You guessed the number in " + numberofGuesses + " tries :)")
 
+#this lets you know that you did not guess the number correctly in seven tries and it tells you what the number is.
 if guess!=number:
 	number=str(number)
 	print("Sorry. The number I was thinking of was " + number + " :)")
